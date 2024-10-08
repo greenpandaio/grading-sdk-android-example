9bc3488b7f946dfadd44e93f55c0ad930d9d7981
Anestis  Kyvranoglou <akivranoglou@gmail.com>
2024-10-08 16:49:08 +0300

    Update to sdk version 0.4.0 with eligibility flow.

--- app/src/main/java/io/pandas/grading_sdk_android_example/MainActivity.kt
+++ app/src/main/java/io/pandas/grading_sdk_android_example/MainActivity.kt
@@ -7,14 +7,16 @@ import android.os.Bundle
 import android.provider.Settings
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.pandas.grading.Grading
+import io.pandas.grading.GradingEventsListener
 import io.pandas.grading.config.ConfigData
 import io.pandas.grading.config.ConfigEvaluationNames
 import io.pandas.grading.config.data_access.Colors
 import io.pandas.grading.config.data_access.DropOffOptions
 import io.pandas.grading.config.data_access.Environment
-import io.pandas.grading.config.data_access.Flows
+import io.pandas.grading.config.data_access.Flow
 import io.pandas.grading.config.data_access.Partner
 
 
@@ -50,32 +52,36 @@ class MainActivity : AppCompatActivity() {
             apply()
         }
     }
-        private fun configureSdk(sessionId: String? = null, flow: Flows = Flows.HOME) {
+        private fun configureSdk(sessionId: String? = null, flow: Flow.Type = Flow.Type.ELIGIBILITY) {
             val backgroundImage = BitmapFactory.decodeResource(resources, R.drawable.pandas_grading_splash_screen)
             val digitizerBackgroundImage = BitmapFactory.decodeResource(resources, R.drawable.pandas_grading_evaluation_digitizer_test_background)
             val config = ConfigData(
                 evaluations = arrayListOf(
+//                    ConfigEvaluationNames.SELF_ASSESSMENT_DEVICE_TURNS_ON,
+//                    SelfAssessmentDecisionEvaluationNames.COSMETIC_FLOW_DECISION,
+//                    ConfigEvaluationNames.COSMETIC_MIRROR,
+                    ConfigEvaluationNames.SELF_ASSESSMENT_LCD_CONDITION,
                     ConfigEvaluationNames.DIGITIZER,
-                    ConfigEvaluationNames.SOUND_PERFORMANCE,
-                    ConfigEvaluationNames.DEVICE_MOTION,
+//                    ConfigEvaluationNames.DEVICE_MOTION,
                     ConfigEvaluationNames.MULTITOUCH,
-                    ConfigEvaluationNames.FRONT_CAMERA,
-                    ConfigEvaluationNames.BACK_CAMERA,
-                    ConfigEvaluationNames.FACE_ID,
+                    ConfigEvaluationNames.SOUND_PERFORMANCE,
+//                    ConfigEvaluationNames.FRONT_CAMERA,
+//                    ConfigEvaluationNames.BACK_CAMERA,
+//                    ConfigEvaluationNames.FACE_ID,
                 ),
                 colors = Colors(
-                    primary = "#222222",
-//                    background = "#427ef5", //defaults to primary
-//                    loader = "#427ef5" //defaults to #B3B3B3
+                    primary = "#E20074",
+                    background = "#FFFFFF", //defaults to primary
+                    loader = "#000000" //defaults to #B3B3B3
                 ),
                 partner = Partner(
                     id = "eb7c5e49-a4af-4426-93e4-4d1dd800b9ad",
-                    name = "greenpandas",
-                    code = "green_panda",
+                    name = "pandas",
+                    code = "pandas",
                     storeLocationsURL = "https://www.pandas.io/el-GR/map"
                 ),
                 environment = Environment.STAGING,
-                flow = flow,
+                flowType = flow,
                 sessionId = sessionId,
                 deviceImei = "352836110046381",
                 dropOffOptions = arrayListOf(
@@ -86,7 +92,13 @@ class MainActivity : AppCompatActivity() {
                 backgroundImage = backgroundImage,
                 digitizerBackgroundImage = digitizerBackgroundImage
             )
-            Grading.setConfig(applicationContext, config)
+            Grading.setConfig(applicationContext, config, eventsListener = object :
+                GradingEventsListener {
+                override fun onEvent(event: GradingEventsListener.Event) {
+                    // TODO handle events
+                    Toast.makeText(applicationContext, "TODO handle event: $event", Toast.LENGTH_SHORT).show()
+                }
+            })
         }
 
         private fun initPandasGradingDeeplinkConfig(): Boolean {
@@ -102,7 +114,7 @@ class MainActivity : AppCompatActivity() {
                     if (uriSegments[0] == "grade") {
                         val partnerId = uriSegments[1]
                         val sessionId = deepLinkUri.getQueryParameter("sessionId")
-                        configureSdk(sessionId, Flows.STORE)
+                        configureSdk(sessionId, Flow.Type.ELIGIBILITY)
                         Grading.start(this)
                     }
                 }
